b7cde31770c0889e5024813d426b02b087059f3c
Anita <anita.mueller@freenet.de>
2019-06-10 23:45:31 +0200

    Konsolen-Fehler behoben bzgl. des dynamischen Textfeldes

--- views/script/gesamt.js
+++ views/script/gesamt.js
@@ -214,28 +214,33 @@ function switchInfoDisplayNavBar(source) {
 }
 
 
-// Dynamisch anpassbare Textareas
-var comfyText = (function(){
-	var tag = document.querySelectorAll('textarea')
-	for (var i=0; i<tag.length; i++){
-		tag[i].addEventListener('paste',autoExpand)
-		tag[i].addEventListener('input',autoExpand)
-		tag[i].addEventListener('keyup',autoExpand)
-	}
-	function autoExpand(e,el){
-		var el = el || e.target
-		el.style.height = 'inherit'
-		el.style.height = el.scrollHeight+'px'
-	}
-	window.addEventListener('load',expandAll)
-	window.addEventListener('resize',expandAll)
-	function expandAll(){
-		var tag = document.querySelectorAll('textarea')
-		for (var i=0; i<tag.length; i++){
-		  autoExpand(e,tag[i])
-		}
-	}
-})()
+// Dynamisch anpassbare Textareas:
+var autoExpand = function (field) {
+	// Reset field height
+	field.style.height = 'inherit';
+
+	// Get the computed styles for the element
+	var computed = window.getComputedStyle(field);
+
+	// Calculate the height
+	var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
+	             + parseInt(computed.getPropertyValue('padding-top'), 10)
+	             + field.scrollHeight
+	             + parseInt(computed.getPropertyValue('padding-bottom'), 10)
+	             + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+
+	
+	// limitation for Expand
+	var limit = 200;
+	field.style.height = Math.min(height, limit) + "px";
+				 
+	//field.style.height = height + 'px';
+};
+
+document.addEventListener('input', function (event) {
+	if (event.target.tagName.toLowerCase() !== 'textarea') return;
+	autoExpand(event.target);
+}, false);
 
 
 
